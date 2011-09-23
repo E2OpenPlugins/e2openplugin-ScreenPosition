1ad73e296a8f15e2ba3550883daaf075b147d8c5
Sjaaky <sjaaky@users.sourceforge.net>
2011-09-23 15:51:19 +0200

    prepare for e2openplugins

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup, Extension
+
+pkg = 'Extensions.ScreenPosition'
+setup (name = 'enigma2-plugin-extensions-screenposition',
+       version = '0.1',
+       description = 'ScreenPosition',
+       packages = [pkg],
+       package_dir = {pkg: 'plugin'}
+      )
